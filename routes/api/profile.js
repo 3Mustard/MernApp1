713218783cdb53242011f888bcdc35b76818eb2f
3Mustard <m23.cullen@gmail.com>
2020-10-12 13:09:53 -0400

    add route to delete a user and their profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -160,4 +160,28 @@ router.post(
   }
 );
 
+// @route   DELETE api/profile/
+// @desc    delete profile, user and posts
+// @access  Private
+router.get(
+  // ROUTE
+  '/',
+  // MIDDLEWARE
+  auth, 
+  // CALLBACK
+  async (req, res) => {
+    try {
+      // remove profile
+      await Profile.findOneAndRemove({ user: req.user.id });
+      // remove user
+      await User.findOneAndRemove({ _id: req.user.id });
+
+      res.json({ msg: 'User deleted' });
+    } catch(err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
 module.exports = router;
