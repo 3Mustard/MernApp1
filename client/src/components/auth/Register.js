9b581604cb9dc71983c8dc53792a707ed4343d3e
3Mustard <m23.cullen@gmail.com>
2020-11-23 17:06:43 -0500

    add html to register.js

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,58 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
   return (
-    <div>
-      Register
-    </div>
-  )
-}
+    <Fragment>
+      <h1 className="large text-primary">
+      Sign Up
+      </h1>
+      <p className="lead">
+        <i className="fas fa-user"></i>
+        Create Your Account
+      </p>
+      <form action="dashboard.html" className="form">
+        <div className="form-group">
+          <input 
+            type="text" 
+            placeholder="Name" 
+            required 
+          />
+        </div>
+        <div className="form-group">
+          <input 
+            type="email" 
+            placeholder="Email" 
+          />
+          <small className="form-text">
+            This site uses Gravatar, use an associated email if you want a Profile Image.
+          </small>
+        </div>
+        <div className="form-group">
+          <input 
+            type="password" 
+            placeholder="Password" 
+            minlength="6" 
+          />
+        </div>
+        <div className="form-group">
+          <input 
+            type="password" 
+            placeholder="Confirm Password" 
+            minlength="6" 
+          />
+        </div>
+        <input 
+          type="submit" 
+          value="Register" 
+          className="btn btn-primary" 
+        />
+      </form>
+      <p className="my-1">
+        Already have an account? <Link to="/login">Log in</Link>
+      </p>
+    </Fragment>
+  );
+};
 
 export default Register;
