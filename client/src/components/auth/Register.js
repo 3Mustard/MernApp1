1a720444da8212bf7590adf54fbe179686563ad1
3Mustard <m23.cullen@gmail.com>
2020-12-04 17:26:34 -0500

    create Alert component

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,11 @@
 import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert'; // takes two params: a message and an alert type
+import PropTypes from 'prop-types';
 
-const Register = () => {
+const Register = ({setAlert}) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -18,8 +21,7 @@ const Register = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
-      // setAlert('Passwords do not match', 'danger');
-      console.log('no match');
+      setAlert('Passwords do not match', 'danger');
     } else {
       const newUser = {
         name,
@@ -100,4 +102,8 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+}
+
+export default connect(null, { setAlert })(Register);
